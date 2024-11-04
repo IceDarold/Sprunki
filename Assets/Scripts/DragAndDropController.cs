6af60167470234aea656d87f413be7dcc012d58c
Youggood <ymnik50s@gmail.com>
2024-11-04 14:40:52 +0300

    add drag and drop

---
+++ Assets/Scripts/DragAndDropController.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using UnityEngine;
+
+
+public  class DragAndDropController : MonoBehaviour
+{
+    private static DragAndDropController instance;
+    private SkinImageObject obj;
+    private SkinSO skinSO;
+
+    private Vector3 mousePosition;
+
+    private void Awake()
+    {
+        instance = this;
+        
+    }
+
+    private void Update()
+    {
+        if(obj != null && Input.GetMouseButtonUp(0))
+        {
+            obj.Return();
+            obj = null;
+            skinSO = null;
+        }
+
+        if(Input.mousePosition != mousePosition && obj != null)
+        {
+            Vector3 delta = Input.mousePosition - mousePosition;
+            obj.transform.position += delta;
+            mousePosition = Input.mousePosition;
+        }
+    }
+    public static void AddObject(SkinImageObject o,SkinSO so)
+    {
+        instance.obj = o;
+        instance.skinSO = so;
+        instance.mousePosition = Input.mousePosition;
+    }
+}
